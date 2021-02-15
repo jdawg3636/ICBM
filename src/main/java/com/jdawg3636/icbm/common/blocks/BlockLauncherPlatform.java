1c5f38529c3eacd84d7c0eb39e7759caedf226c2
jdawg3636 <jdawg3636minecraft@gmail.com>
2021-02-15 14:52:40 -0600

    Multiblock Destruction Routine A little hacky but fine for the time being - item drops nyi

--- src/main/java/com/jdawg3636/icbm/common/blocks/BlockLauncherPlatform.java
+++ src/main/java/com/jdawg3636/icbm/common/blocks/BlockLauncherPlatform.java
@@ -14,18 +14,21 @@ import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.IntegerProperty;
 import net.minecraft.state.StateContainer;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.vector.Vector3i;
-import net.minecraft.world.IWorld;
-import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
 
 public class BlockLauncherPlatform extends Block {
+
+    public static final Vector3i[] MULTIBLOCK_POSITIONS = {new Vector3i(1,0,0), new Vector3i(1,1,0), new Vector3i(1,2,0), new Vector3i(-1,0,0), new Vector3i(-1,1,0), new Vector3i(-1,2,0)};
+
+    public Vector3i[] getMultiblockPositions() {
+        return MULTIBLOCK_POSITIONS;
+    }
 
     /**
      * Properties for Positioning Within Multiblock
@@ -82,132 +85,6 @@ public class BlockLauncherPlatform extends Block {
     }
 
     /**
-     * Multiblock Placement Routine
-     *
-     * Called by ItemBlocks after a block is set in the world, to allow post-place logic
-     */
-    @Override
-    public void onBlockPlacedBy(World worldIn, BlockPos pos, BlockState state, LivingEntity placer, ItemStack stack) {
-
-        Vector3i[] multiblockPositions = {new Vector3i(1,0,0), new Vector3i(1,1,0), new Vector3i(1,2,0), new Vector3i(-1,0,0), new Vector3i(-1,1,0), new Vector3i(-1,2,0)};
-
-        for(Vector3i multiblockPos : multiblockPositions) {
-
-            // Calculate Rotation based on "facing" property
-            // NOTE: Using opposite of "facing", so all offsets are from pov of the player
-            Vector3i multiblockPosRotated;
-
-            // North (-z) (Default)
-            multiblockPosRotated = multiblockPos;
-            // East (+x)
-            if(state.get(FACING).getOpposite().getDirectionVec().getX() == 1)
-                multiblockPosRotated = new Vector3i(+multiblockPos.getZ(), multiblockPos.getY(), +multiblockPos.getX());
-            // South (+z)
-            else if(state.get(FACING).getOpposite().getDirectionVec().getZ() == 1)
-                multiblockPosRotated = new Vector3i(-multiblockPos.getX(), multiblockPos.getY(), -multiblockPos.getZ());
-            // West (-x)
-            else if(state.get(FACING).getOpposite().getDirectionVec().getX() == -1)
-                multiblockPosRotated = new Vector3i(+multiblockPos.getZ(), multiblockPos.getY(), -multiblockPos.getX());
-
-            worldIn.setBlockState(
-                    // Use rotated offset + base coords for world placement
-                    pos.add(multiblockPosRotated.getX(), multiblockPosRotated.getY(), multiblockPosRotated.getZ()),
-                    // Encode unrotated offset into BlockState
-                    this.getDefaultState()
-                            .with(FACING, state.get(FACING))
-                            .with(MULTIBLOCK_OFFSET_HORIZONTAL, Math.abs(multiblockPos.getX()))
-                            .with(MULTIBLOCK_OFFSET_HORIZONTAL_NEGATIVE, multiblockPos.getX() < 0)
-                            .with(MULTIBLOCK_OFFSET_HEIGHT, Math.abs(multiblockPos.getY()))
-                            .with(MULTIBLOCK_OFFSET_HEIGHT_NEGATIVE, multiblockPos.getY() < 0)
-                            .with(MULTIBLOCK_OFFSET_DEPTH, Math.abs(multiblockPos.getZ()))
-                            .with(MULTIBLOCK_OFFSET_DEPTH_NEGATIVE, multiblockPos.getZ() < 0)
-                    , 3);
-        }
-
-    }
-
-    @Override
-    public boolean isValidPosition(BlockState state, IWorldReader worldIn, BlockPos pos) {
-        //TODO temp bypass, need to implement
-        if (true /*TODO|| state.get(HALF) != DoubleBlockHalf.UPPER*/) {
-            return super.isValidPosition(state, worldIn, pos);
-        } else {
-            BlockState blockstate = worldIn.getBlockState(pos.down());
-            if (state.getBlock() != this) return super.isValidPosition(state, worldIn, pos); //Forge: This function is called during world gen and placement, before this block is set, so if we are not 'here' then assume it's the pre-check.
-            return blockstate.isIn(this) /*TODO&& blockstate.get(HALF) == DoubleBlockHalf.LOWER*/;
-        }
-    }
-
-    /**
-     * Initiates Multiblock Destruction Propagation
-     *
-     * Called before the Block is set to air in the world. Called regardless of if the player's tool can actually collect
-     * this block
-     */
-    @Override
-    public void onBlockHarvested(World worldIn, BlockPos pos, BlockState state, PlayerEntity player) {
-        if (!worldIn.isRemote) {
-            if (player.isCreative()) {
-                removeBottomHalf(worldIn, pos, state, player);
-            } else {
-                spawnDrops(state, worldIn, pos, (TileEntity)null, player, player.getHeldItemMainhand());
-            }
-        }
-
-        super.onBlockHarvested(worldIn, pos, state, player);
-    }
-
-    /**
-     * Multiblock Destruction Impl Util
-     *
-     * Only called if broken in Creative Mode, otherwise uses spawnDrops method
-     */
-    protected static void removeBottomHalf(World world, BlockPos pos, BlockState state, PlayerEntity player) {
-        /*TODO
-        DoubleBlockHalf doubleblockhalf = state.get(HALF);
-        if (doubleblockhalf == DoubleBlockHalf.UPPER) {
-            BlockPos blockpos = pos.down();
-            BlockState blockstate = world.getBlockState(blockpos);
-            if (blockstate.getBlock() == state.getBlock() && blockstate.get(HALF) == DoubleBlockHalf.LOWER) {
-                world.setBlockState(blockpos, Blocks.AIR.getDefaultState(), 35);
-                world.playEvent(player, 2001, blockpos, Block.getStateId(blockstate));
-            }
-        }
-         */
-    }
-
-    /**
-     * Currently using this to propagate multiblock destruction. This might be a bad idea.
-     *
-     * Update the provided state given the provided neighbor facing and neighbor state, returning a new state.
-     * For example, fences make their connections to the passed in state if possible, and wet concrete powder immediately
-     * returns its solidified counterpart.
-     * Note that this method should ideally consider only the specific face passed in.
-     */
-    @Override
-    public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos) {
-        //todo temp bypass
-        /*if(1==1)*/ return super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
-        /*DoubleBlockHalf doubleblockhalf = stateIn.get(HALF);
-        if (facing.getAxis() != Direction.Axis.Y || doubleblockhalf == DoubleBlockHalf.LOWER != (facing == Direction.UP) || facingState.isIn(this) && facingState.get(HALF) != doubleblockhalf) {
-            return doubleblockhalf == DoubleBlockHalf.LOWER && facing == Direction.DOWN && !stateIn.isValidPosition(worldIn, currentPos) ? Blocks.AIR.getDefaultState() : super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
-        } else {
-            return Blocks.AIR.getDefaultState();
-        }*/
-    }
-
-    /**
-     * TODO Implement Custom Drop Routine
-     *
-     * Spawns the block's drops in the world. By the time this is called the Block has possibly been set to air via
-     * Block.removedByPlayer
-     */
-    @Override
-    public void harvestBlock(World worldIn, PlayerEntity player, BlockPos pos, BlockState state, @Nullable TileEntity te, ItemStack stack) {
-        super.harvestBlock(worldIn, player, pos, Blocks.AIR.getDefaultState(), te, stack);
-    }
-
-    /**
      * Override to take into account the {@link this.FACING} property
      *
      * Copied from net.minecraft.block.AnvilBlock
@@ -223,6 +100,121 @@ public class BlockLauncherPlatform extends Block {
     @Override
     public PushReaction getPushReaction(BlockState state) {
         return PushReaction.BLOCK;
+    }
+
+    /**
+     * Multiblock Placement Routine
+     *
+     * Called by ItemBlocks after a block is set in the world, to allow post-place logic
+     */
+    @Override
+    public void onBlockPlacedBy(World worldIn, BlockPos pos, BlockState state, LivingEntity placer, ItemStack stack) {
+        super.onBlockPlacedBy(worldIn, pos, state, placer, stack);
+        fillMultiblock(worldIn, pos, state, false);
+    }
+
+    /**
+     * Initiates Multiblock Destruction Propagation
+     *
+     * Called before the Block is set to air in the world. Called regardless of if the player's tool can actually collect
+     * this block
+     */
+    @Override
+    public void onBlockHarvested(World worldIn, BlockPos pos, BlockState state, PlayerEntity player) {
+        super.onBlockHarvested(worldIn, pos, state, player);
+        destroyMultiblock(worldIn, pos, state);
+    }
+
+    public void fillMultiblock(World worldIn, BlockPos pos, BlockState state, boolean setToAir) {
+
+        // Add Center Block to List
+        Vector3i[] multiblockPositions = new Vector3i[getMultiblockPositions().length+1];
+        for(int i = 0; i < getMultiblockPositions().length; i++) multiblockPositions[i] = getMultiblockPositions()[i];
+        multiblockPositions[multiblockPositions.length-1] = new Vector3i(0, 0, 0);
+
+        for(Vector3i multiblockPos : multiblockPositions) {
+
+            // Calculate Rotation based on "facing" property
+            // NOTE: Using opposite of "facing", so all offsets are from pov of the player
+            Vector3i multiblockPosRotated;
+
+            // North (-z) (Default)
+            multiblockPosRotated = multiblockPos;
+            // East (+x)
+            if(state.get(FACING).getOpposite().getDirectionVec().getX() == 1)
+                multiblockPosRotated = new Vector3i(+multiblockPos.getZ(), multiblockPos.getY(), +multiblockPos.getX());
+                // South (+z)
+            else if(state.get(FACING).getOpposite().getDirectionVec().getZ() == 1)
+                multiblockPosRotated = new Vector3i(-multiblockPos.getX(), multiblockPos.getY(), -multiblockPos.getZ());
+                // West (-x)
+            else if(state.get(FACING).getOpposite().getDirectionVec().getX() == -1)
+                multiblockPosRotated = new Vector3i(+multiblockPos.getZ(), multiblockPos.getY(), -multiblockPos.getX());
+
+            // Use rotated offset + base coords for world placement
+            BlockPos worldPos = pos.add(multiblockPosRotated.getX(), multiblockPosRotated.getY(), multiblockPosRotated.getZ());
+
+            if(worldIn.getBlockState(new BlockPos(worldPos)).getBlock().getDefaultState().equals(getDefaultState()) || worldIn.getBlockState(new BlockPos(worldPos)).getMaterial().isReplaceable()) {
+
+                if(setToAir){
+                    worldIn.setBlockState(worldPos, Blocks.AIR.getDefaultState());
+                }
+                else {
+                    worldIn.setBlockState(
+                            worldPos,
+                            // Encode unrotated offset into BlockState
+                            this.getDefaultState()
+                                    .with(FACING, state.get(FACING))
+                                    .with(MULTIBLOCK_OFFSET_HORIZONTAL, Math.abs(multiblockPos.getX()))
+                                    .with(MULTIBLOCK_OFFSET_HORIZONTAL_NEGATIVE, multiblockPos.getX() < 0)
+                                    .with(MULTIBLOCK_OFFSET_HEIGHT, Math.abs(multiblockPos.getY()))
+                                    .with(MULTIBLOCK_OFFSET_HEIGHT_NEGATIVE, multiblockPos.getY() < 0)
+                                    .with(MULTIBLOCK_OFFSET_DEPTH, Math.abs(multiblockPos.getZ()))
+                                    .with(MULTIBLOCK_OFFSET_DEPTH_NEGATIVE, multiblockPos.getZ() < 0)
+                            , 3
+                    );
+                }
+
+            }
+
+        }
+
+    }
+
+    /**
+     * Multiblock Destruction Routine
+     */
+    public void destroyMultiblock(World worldIn, BlockPos pos, BlockState sourceState) {
+
+        // Raw Data from BlockState
+        int offsetX = sourceState.get(MULTIBLOCK_OFFSET_HORIZONTAL); if(sourceState.get(MULTIBLOCK_OFFSET_HORIZONTAL_NEGATIVE)) offsetX *= -1;
+        int offsetY = sourceState.get(MULTIBLOCK_OFFSET_HEIGHT); if(sourceState.get(MULTIBLOCK_OFFSET_HEIGHT_NEGATIVE)) offsetY *= -1;
+        int offsetZ = sourceState.get(MULTIBLOCK_OFFSET_DEPTH); if(sourceState.get(MULTIBLOCK_OFFSET_DEPTH_NEGATIVE)) offsetZ *= -1;
+        // North (-z)
+        if(sourceState.get(FACING).getDirectionVec().getZ() == -1) {
+            offsetX *= -1;
+        }
+        // East (+x)
+        if(sourceState.get(FACING).getDirectionVec().getX() == 1) {
+            int temp = offsetX;
+            offsetX = -offsetZ;
+            offsetZ = -temp;
+        }
+        // South (+z)
+        else if(sourceState.get(FACING).getDirectionVec().getZ() == 1) {
+            offsetZ *= -1;
+        }
+        // West (-x)
+        else if(sourceState.get(FACING).getDirectionVec().getX() == -1) {
+            int temp = offsetX;
+            offsetX = offsetZ;
+            offsetZ = temp;
+        }
+
+        // Invert Offsets
+        BlockPos posOfCenter = pos.add(-offsetX, -offsetY, -offsetZ);
+        // Fill with Air
+        fillMultiblock(worldIn, posOfCenter, worldIn.getBlockState(posOfCenter), true);
+
     }
 
 }
