4a213cac658df7314376e3cc146e142158d385f0
jdawg3636 <jdawg3636minecraft@gmail.com>
2021-06-29 00:44:10 -0500

    Remove Old Commented-Out Code
    Had been commented out from the 1.12 version as a reference, long-since irrelevant.

--- src/main/java/com/jdawg3636/icbm/common/reg/EntityReg.java
+++ src/main/java/com/jdawg3636/icbm/common/reg/EntityReg.java
@@ -117,47 +117,4 @@ public final class EntityReg {
         );
     }
 
-    /*
-
-    private static int nextEntityID = 0;
-
-    @SubscribeEvent
-    public static void registerEntity(RegistryEvent.Register<EntityEntry> event) {
-
-        event.getRegistry().register(buildEntityEntry(EntityFlyingBlock.class, ICBMEntities.BLOCK_GRAVITY, 128, 15));
-        event.getRegistry().register(buildEntityEntry(EntityFragments.class, ICBMEntities.BLOCK_FRAGMENT, 40, 8));
-        event.getRegistry().register(buildEntityEntry(EntityExplosive.class, ICBMEntities.BLOCK_EXPLOSIVE, 50, 5));
-        event.getRegistry().register(buildEntityEntry(EntityMissile.class, ICBMEntities.MISSILE, 500, 1));
-        event.getRegistry().register(buildEntityEntry(EntityExplosion.class, ICBMEntities.EXPLOSION, 100, 5));
-        event.getRegistry().register(buildEntityEntry(EntityLightBeam.class, ICBMEntities.BEAM, 80, 5));
-        event.getRegistry().register(buildEntityEntry(EntityGrenade.class, ICBMEntities.GRENADE, 50, 5));
-        event.getRegistry().register(buildEntityEntry(EntityBombCart.class, ICBMEntities.BOMB_CART, 50, 2));
-        event.getRegistry().register(buildEntityEntry(EntityPlayerSeat.class, ICBMEntities.MISSILE_SEAT, 50, 2));
-
-        //Green team
-        event.getRegistry().register(buildMobEntry(EntityXmasSkeleton.class, "skeleton.xmas.elf", Color.GREEN, Color.CYAN));
-        event.getRegistry().register(buildMobEntry(EntityXmasSkeletonBoss.class, "skeleton.xmas.boss", Color.GREEN, Color.CYAN));
-        event.getRegistry().register(buildMobEntry(EntityXmasSnowman.class, "skeleton.xmas.snowman", Color.BLACK, Color.CYAN));
-
-        //Red team
-        event.getRegistry().register(buildMobEntry(EntityXmasZombie.class, "zombie.xmas.elf", Color.RED, Color.CYAN));
-        event.getRegistry().register(buildMobEntry(EntityXmasZombieBoss.class, "zombie.xmas.boss", Color.RED, Color.CYAN));
-        event.getRegistry().register(buildMobEntry(EntityXmasCreeper.class, "zombie.xmas.creeper", Color.RED, Color.CYAN));
-
-
-        event.getRegistry().register(buildEntityEntry(EntityXmasRPG.class, "skeleton.snowman.rocket", 64, 1));
-
-    }
-
-    private static EntityEntry buildEntityEntry(Class<? extends Entity> entityClass, ResourceLocation name, int trackingRange, int updateFrequency) {
-        EntityEntryBuilder builder = EntityEntryBuilder.create();
-        builder.name(name.toString());
-        builder.id(name, nextEntityID++);
-        builder.tracker(trackingRange, updateFrequency, true);
-        builder.entity(entityClass);
-        return builder.build();
-    }
-
-    */
-
 }
