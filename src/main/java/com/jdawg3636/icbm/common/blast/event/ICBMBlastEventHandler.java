62815429e8188221727fa442893e77ca5d4a9ede
jdawg3636 <jdawg3636minecraft@gmail.com>
2021-10-23 18:02:31 -0500

    Add Highly WIP Nuclear Explosion, Tweak Sonic Explosions

--- src/main/java/com/jdawg3636/icbm/common/blast/event/ICBMBlastEventHandler.java
+++ src/main/java/com/jdawg3636/icbm/common/blast/event/ICBMBlastEventHandler.java
@@ -1,7 +1,9 @@
 package com.jdawg3636.icbm.common.blast.event;
 
+import com.jdawg3636.icbm.ICBMReference;
 import com.jdawg3636.icbm.common.entity.EntityLingeringBlast;
 import com.jdawg3636.icbm.common.entity.EntityShrapnel;
+import com.jdawg3636.icbm.common.reg.BlockReg;
 import com.jdawg3636.icbm.common.reg.EntityReg;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
@@ -12,12 +14,14 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.network.play.server.SExplosionPacket;
 import net.minecraft.particles.ParticleTypes;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.Explosion;
+import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 import java.util.ArrayList;
@@ -29,11 +33,11 @@ import java.util.ArrayList;
 public class ICBMBlastEventHandler {
 
     public static void doBlastSoundAndParticles(BlastEvent event) {
-        // Loosely Based on net.minecraft.world.Explosion.doExplosionB(boolean spawnParticles)
+        // Loosely Based on net.minecraft.world.Explosion::finalizeExplosion (MCP Class Names and Package Structure, Official Method/Field Mappings, Minecraft 1.16.5)
         if (!event.getBlastWorld().isClientSide) {
             // Sound
             event.getBlastWorld().playSound((PlayerEntity) null, event.getBlastPosition().getX(), event.getBlastPosition().getY(), event.getBlastPosition().getZ(), SoundEvents.GENERIC_EXPLODE, SoundCategory.BLOCKS, 4.0F, (1.0F + (event.getBlastWorld().random.nextFloat() - event.getBlastWorld().random.nextFloat()) * 0.2F) * 0.7F);
-            // Particles - Handled Client Side by net.minecraft.client.network.play.ClientPlayNetHandler.handleParticles(SSpawnParticlePacket packetIn)
+            // Particles - Handled Client Side by net.minecraft.client.network.play.ClientPlayNetHandler::handleParticleEvent (MCP Class Names and Package Structure, Official Method/Field Mappings, Minecraft 1.16.5)
             event.getBlastWorld().sendParticles(ParticleTypes.EXPLOSION_EMITTER, event.getBlastPosition().getX(), event.getBlastPosition().getY(), event.getBlastPosition().getZ(), 1, 0, 0, 0, 1.0D);
         }
     }
@@ -43,7 +47,28 @@ public class ICBMBlastEventHandler {
     }
 
     public static void doVanillaExplosion(BlastEvent event, float explosionPower) {
-        event.getBlastWorld().explode(null, event.getBlastPosition().getX(), event.getBlastPosition().getY(), event.getBlastPosition().getZ(), explosionPower, Explosion.Mode.BREAK);
+        doVanillaExplosion(event.getBlastWorld(), event.getBlastPosition().getX(), event.getBlastPosition().getY(), event.getBlastPosition().getZ(), explosionPower);
+    }
+
+    public static void doVanillaExplosion(ServerWorld level, BlockPos blockPos, float explosionPower) {
+        level.explode(null, blockPos.getX(), blockPos.getY(), blockPos.getZ(), explosionPower, Explosion.Mode.BREAK);
+    }
+
+    public static void doVanillaExplosion(ServerWorld level, double posX, double posY, double posZ, float explosionPower) {
+        level.explode(null, posX, posY, posZ, explosionPower, Explosion.Mode.BREAK);
+    }
+
+    public static void doVanillaExplosionServerOnly(ServerWorld level, BlockPos blockPos, float explosionPower) {
+        doVanillaExplosionServerOnly(level, blockPos.getX(), blockPos.getY(), blockPos.getZ(), explosionPower);
+    }
+
+    public static void doVanillaExplosionServerOnly(ServerWorld level, double posX, double posY, double posZ, float explosionPower) {
+        Explosion explosion = new Explosion(level, null, null, null, posX, posY, posZ, explosionPower, false, Explosion.Mode.BREAK);
+        if (net.minecraftforge.event.ForgeEventFactory.onExplosionStart(level, explosion)) {
+            return;
+        }
+        explosion.explode();
+        explosion.finalizeExplosion(false);
     }
 
     @SubscribeEvent
@@ -52,7 +77,7 @@ public class ICBMBlastEventHandler {
         doVanillaExplosion(event);
         for(double i = -0.5; i <= 0.5; i += 0.0625) {
             for(double j = -0.5; j <= 0.5; j += 0.0625) {
-                if(i * i + j * j > 0.5 * 0.5) continue; // Circle, not a square.
+                if(i * i + j * j > 0.5 * 0.5) continue; // Truncate to Circle from Square
                 FallingBlockEntity fallingblockentity = new FallingBlockEntity(event.getBlastWorld(), (double)event.getBlastPosition().getX() + 0.5D, (double)event.getBlastPosition().getY() + 1, (double)event.getBlastPosition().getZ() + 0.5D, Blocks.DAMAGED_ANVIL.defaultBlockState());
                 fallingblockentity.time = 1;
                 fallingblockentity.cancelDrop = true;
@@ -258,7 +283,7 @@ public class ICBMBlastEventHandler {
 
         // Affect Blocks
         for(BlockPos blockPos : results) {
-            FallingBlockEntity fallingBlockEntity = new FallingBlockEntity(event.getBlastWorld(), blockPos.getX(), blockPos.getY(), blockPos.getZ(), event.getBlastWorld().getBlockState(blockPos));
+            FallingBlockEntity fallingBlockEntity = new FallingBlockEntity(event.getBlastWorld(), blockPos.getX() + 0.5, blockPos.getY(), blockPos.getZ() + 0.5, event.getBlastWorld().getBlockState(blockPos));
             fallingBlockEntity.time = 1;
             fallingBlockEntity.push(0D, 4D, 0D);
             event.getBlastWorld().setBlock(blockPos, Blocks.AIR.defaultBlockState(), 3);
@@ -325,6 +350,35 @@ public class ICBMBlastEventHandler {
     @SubscribeEvent
     public static void onBlastThermobaric(BlastEvent.Thermobaric event) {
         doVanillaExplosion(event, 4F * 4.0F);
+    }
+
+    @SubscribeEvent
+    public static void onBlastNuclear(BlastEvent.Nuclear event) {
+
+        doBlastSoundAndParticles(event);
+
+        if (!event.getBlastWorld().isClientSide) {
+            int radius = 64;
+            for (int explosionPosY = -radius / 8; explosionPosY <= 0; explosionPosY += 8) {
+                for(int explosionPosX = -radius; explosionPosX <= radius; explosionPosX += 8) {
+                    for (int explosionPosZ = -radius; explosionPosZ <= radius; explosionPosZ += 8) {
+                        if(explosionPosX * explosionPosX + explosionPosZ * explosionPosZ + explosionPosY * explosionPosY < radius * radius) {
+                            doVanillaExplosionServerOnly(event.getBlastWorld(), event.getBlastPosition().offset(explosionPosX, explosionPosY, explosionPosZ), 4F * 4.0F);
+                        }
+                    }
+                }
+            }
+            for(int i = -radius - 10; i <= radius + 10; ++i) {
+                for (int j = (-radius / 8) - 10; j <= 0; ++j) {
+                    for (int k = -radius - 10; k <= radius + 10; ++k) {
+                        if(event.getBlastWorld().random.nextFloat() < 0.15 && i * i + j * j + k * k < radius * radius && event.getBlastWorld().getBlockState(event.getBlastPosition().offset(i, j, k)).getBlock().getTags().contains(new ResourceLocation(ICBMReference.MODID, "can_become_radioactive_material"))) {
+                            event.getBlastWorld().setBlock(event.getBlastPosition().offset(i, j, k), BlockReg.RADIOACTIVE_MATERIAL.get().defaultBlockState(), 3);
+                        }
+                    }
+                }
+            }
+        }
+
     }
 
 }
