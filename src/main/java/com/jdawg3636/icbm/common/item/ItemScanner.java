d82aa099bc3f188c4ce5ba3fcace81ed4a80dfc5
jdawg3636 <jdawg3636minecraft@gmail.com>
2022-01-06 13:38:09 -0600

    Add Dispenser Behavior to Scanner

--- src/main/java/com/jdawg3636/icbm/common/item/ItemScanner.java
+++ src/main/java/com/jdawg3636/icbm/common/item/ItemScanner.java
@@ -3,18 +3,23 @@ package com.jdawg3636.icbm.common.item;
 import com.jdawg3636.icbm.ICBMReference;
 import com.jdawg3636.icbm.common.capability.ICBMCapabilities;
 import com.jdawg3636.icbm.common.capability.trackingmanager.ITrackingManagerCapability;
+import net.minecraft.block.DispenserBlock;
+import net.minecraft.dispenser.DefaultDispenseItemBehavior;
+import net.minecraft.dispenser.IBlockSource;
+import net.minecraft.dispenser.IDispenseItemBehavior;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Hand;
-import net.minecraft.util.Util;
+import net.minecraft.util.*;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.fml.server.ServerLifecycleHooks;
+
+import java.util.List;
 
 public class ItemScanner extends Item {
 
@@ -24,6 +29,7 @@ public class ItemScanner extends Item {
 
     public ItemScanner(Item.Properties properties) {
         super(properties.stacksTo(1).tab(ICBMReference.CREATIVE_TAB));
+        DispenserBlock.registerBehavior(this, DISPENSE_ITEM_BEHAVIOR);
     }
 
     @Override
@@ -50,6 +56,33 @@ public class ItemScanner extends Item {
             }
         }
         return ActionResult.sidedSuccess(player.getItemInHand(hand), level.isClientSide());
+    }
+
+    public static final IDispenseItemBehavior DISPENSE_ITEM_BEHAVIOR = new DefaultDispenseItemBehavior() {
+        protected ItemStack execute(IBlockSource blockSource, ItemStack itemStack) {
+            ItemScanner.dispense(blockSource);
+            return itemStack;
+        }
+    };
+
+    public static void dispense(IBlockSource blockSource) {
+
+        BlockPos blockPos = blockSource.getPos().relative((Direction)blockSource.getBlockState().getValue(DispenserBlock.FACING));
+        List<LivingEntity> candidateTargetList = blockSource.getLevel().getEntitiesOfClass(LivingEntity.class, new AxisAlignedBB(blockPos), EntityPredicates.NO_SPECTATORS);
+
+        if (candidateTargetList.isEmpty()) return;
+
+        LivingEntity target = (LivingEntity)candidateTargetList.get(0);
+        World levelOverworld = ServerLifecycleHooks.getCurrentServer().getLevel(World.OVERWORLD);
+        if(levelOverworld == null) return;
+        LazyOptional<ITrackingManagerCapability> cap = levelOverworld.getCapability(ICBMCapabilities.TRACKING_MANAGER_CAPABILITY);
+        if(cap.isPresent()) {
+            cap.orElse(null).clearTickets(target.getUUID());
+            if(target instanceof PlayerEntity) {
+                target.sendMessage(new TranslationTextComponent("chat.icbm.scannerconfirm.self", target.getName()), Util.NIL_UUID);
+            }
+        }
+
     }
 
 }
