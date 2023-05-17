6da27562226ff7ddb5033709b47a5ebd1f103da5
jdawg3636 <jdawg3636minecraft@gmail.com>
2023-05-16 23:03:28 -0500

    Improved Packet Registration using Reflection+Lambda War Crimes

--- src/main/java/com/jdawg3636/icbm/common/network/ICBMNetworking.java
+++ src/main/java/com/jdawg3636/icbm/common/network/ICBMNetworking.java
@@ -1,9 +1,16 @@
 package com.jdawg3636.icbm.common.network;
 
 import com.jdawg3636.icbm.ICBMReference;
+import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.network.NetworkEvent;
 import net.minecraftforge.fml.network.NetworkRegistry;
 import net.minecraftforge.fml.network.simple.SimpleChannel;
+
+import java.lang.reflect.Method;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class ICBMNetworking {
 
@@ -21,13 +28,22 @@ public class ICBMNetworking {
 
         int packetID = 0;
         //noinspection UnusedAssignment
-        INSTANCE.messageBuilder(
-                CPacketUpdateLauncherControlPanel.class, packetID++)
-                .encoder(CPacketUpdateLauncherControlPanel::write)
-                .decoder(CPacketUpdateLauncherControlPanel::read)
-                .consumer(CPacketUpdateLauncherControlPanel::handle)
-                .add();
+        registerPacket(INSTANCE, CPacketUpdateLauncherControlPanel.class, packetID++);
 
+    }
+
+    public static <T> void registerPacket(SimpleChannel channel, Class<T> packetClass, int packetID) {
+        try {
+            final Method writeMethod = packetClass.getDeclaredMethod("write", packetClass, PacketBuffer.class);
+            final Method readMethod = packetClass.getDeclaredMethod("read", PacketBuffer.class);
+            final Method handleMethod = packetClass.getDeclaredMethod("handle", packetClass, Supplier.class);
+            final BiConsumer<T, PacketBuffer> writeInvoker = (packet, buffer) -> { try { writeMethod.invoke(null, packet, buffer); } catch (Throwable t) { throw new RuntimeException(t); } };
+            final Function<PacketBuffer, T> readInvoker = (packet) -> { try { return (T)readMethod.invoke(null, packet); } catch (Throwable t) { throw new RuntimeException(t); } };
+            final BiConsumer<T, Supplier<NetworkEvent.Context>> handleInvoker = (packet, contextSupplier) -> { try { handleMethod.invoke(null, packet, contextSupplier); } catch (Throwable t) { throw new RuntimeException(t); } };
+            channel.messageBuilder(packetClass, packetID).encoder(writeInvoker).decoder(readInvoker).consumer(handleInvoker).add();
+        } catch (Throwable t) {
+            throw new RuntimeException(t);
+        }
     }
 
 }
